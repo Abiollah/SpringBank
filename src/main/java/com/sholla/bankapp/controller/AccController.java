f59a562fcc9e3a8d20b639ae432288e419495c17
Abiolla <faruqbee@gmail.com>
2021-09-09 13:05:04 +0100

    adding swagger

--- src/main/java/com/sholla/bankapp/controller/AccController.java
+++ src/main/java/com/sholla/bankapp/controller/AccController.java
@@ -1,24 +1,20 @@
 package com.sholla.bankapp.controller;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.sholla.bankapp.model.Account;
+
+
 import com.sholla.bankapp.model.BankEntities;
 import com.sholla.bankapp.repository.AccRepository;
-import com.sholla.bankapp.repository.BankRepository;
 import com.sholla.bankapp.response.ResponseHandler;
 
 
@@ -29,19 +25,11 @@ public class AccController {
 	@Autowired
 	private AccRepository service;
 	
-//	@Autowired
-//	BankRepository bank;
+//	
 	
 	@PostMapping("/account")
 	public ResponseEntity<Object> createUser( @RequestBody BankEntities acc) {
-		//Account savedAcc = service.AddAcc(acc);
 		
-//		URI location = ServletUriComponentsBuilder
-//			.fromCurrentRequest()
-//			.path("/{accountName}")
-//			.buildAndExpand(savedUser.getAccountName()).toUri();
-//		
-//		return ResponseEntity.created(location).build();
 		
 		try {
 			BankEntities savedAcc = service.AddAcc(acc);
@@ -67,29 +55,16 @@ public class AccController {
 	}
 	
 	
-	@PostMapping("/tutorials")
-	public ResponseEntity<BankEntities> createTutorial(@RequestBody BankEntities tutorial) {
+	@PostMapping("/first_acc")
+	public ResponseEntity<BankEntities> createTutorial(@RequestBody BankEntities acc) {
 		try {
-			BankEntities _tutorial = service
-					.AddAcc(new BankEntities(tutorial.getAccountName(), tutorial.getAccountPassword(), tutorial.getInitialDeposit()));
-			return new ResponseEntity<>(_tutorial, HttpStatus.CREATED);
+			BankEntities account = service.AddAcc(acc);
+			return new ResponseEntity<>(account, HttpStatus.CREATED);
 		} catch (Exception e) {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 	
-//	@PutMapping("/deposit/{acctID}/withdraw/{amount}")
-//	public ResponseEntity<Object> deposit(@PathVariable String accountName, @PathVariable double amount) {
-//		
-//		
-//		try {
-//			Account savedAcc = bank.Deposit(accountName, amount);
-//			return ResponseHandler.generateResponse(200, HttpStatus.OK, true, "Account retrived Successfully", savedAcc);
-//		} catch (Exception e) {
-//			return ResponseHandler.generateResponse(200, HttpStatus.MULTI_STATUS, false, null, null);
-//		}
-//		
-//		
-//	}
+
 	
 }
--- src/main/java/com/sholla/bankapp/controller/DepositController.java
+++ src/main/java/com/sholla/bankapp/controller/DepositController.java
@@ -10,21 +10,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.sholla.bankapp.model.BankEntities;
 import com.sholla.bankapp.repository.DepositRepository;
 import com.sholla.bankapp.response.ResponseHandler;
 
-
 @RestController
 @RequestMapping({ "/api/v1" })
 public class DepositController {
-	
+
 	@Autowired
 	private DepositRepository depservice;
-	
-	
-	
+
 	@GetMapping("/deposit")
 	public ResponseEntity<Object> acc() {
 		try {
@@ -33,18 +31,26 @@ public class DepositController {
 		} catch (Exception e) {
 			return ResponseHandler.generateResponse(200, HttpStatus.MULTI_STATUS, false, null);
 		}
-		
+
 	}
-	
-	
+
 	@PostMapping("/deposit")
 	public ResponseEntity<Object> createdeposit(@RequestBody BankEntities deposit) {
+		
+		if (deposit.getAmount() >= 100000)
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Amount not accepted");
+		else {
+			if (deposit.getAmount() < 1.0)
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Amount rejected");
+		}
+
 		try {
-			BankEntities _deposit = depservice.AddDeposit(new BankEntities(deposit.getAccountNumber(), deposit.getAmount()));
+			BankEntities _deposit = depservice
+					.AddDeposit(new BankEntities(deposit.getAccountNumber(), deposit.getAmount()));
 			return ResponseHandler.generateResponse(200, HttpStatus.OK, true, "Deposit made Successfully");
 		} catch (Exception e) {
 			return ResponseHandler.generateResponse(200, HttpStatus.MULTI_STATUS, false, null);
 		}
 	}
-	
+
 }
